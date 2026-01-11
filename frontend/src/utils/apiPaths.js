205d4f2b96a9feb563d99e23df69bd98529ed2ae
Vishal Kumar <128839643+Vishal586@users.noreply.github.com>
2026-01-11 14:29:35 +0530

    Update apiPaths.js

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://ai-invoice-app-1-backend.onrender.com";
+export const BASE_URL = "https://ai-invoice-app-backend2.onrender.com/";
 
 export const API_PATHS = {
     AUTH: {
@@ -23,4 +23,5 @@ export const API_PATHS = {
     }
 
 };
+
 
