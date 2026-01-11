a34e142bfcf6ef1f715a6b91d86a54083cb87b48
Vishal Kumar <128839643+Vishal586@users.noreply.github.com>
2026-01-11 14:11:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const authRoutes = require("./routes/authRoutes");
 const invoiceRoutes = require("./routes/invoiceRoutes");
@@ -54,6 +55,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
